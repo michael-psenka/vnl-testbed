32e3807a7e315a8580c2ed2c561bf67e99621cb1
michael-psenka <michael.psenka@gmail.com>
2024-02-15 17:42:07 -0800

    add plotting code

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from models.parent import Autoencoder
 from type_shorthands import *
 from data import multimnist
 
+import matplotlib.pyplot as plt
+
 # Define the loss function
 def mse_loss(params: Dict[str, float], model: Autoencoder, batch: R_bxdxdxc):
     # when declaring mutable, model.apply returns tuple, with first element
@@ -57,3 +59,42 @@ for epoch in range(num_epochs):
         state, loss = train_step(state, batch)
 
     print(f"Epoch {epoch}, Loss: {loss}")
+
+# print reconstruction quality
+# take a random image, and display (i) the image, (ii) the reconstruction, and (iii) the individual reconstructions for all blocks
+image = images[0]
+reconstruction = model.apply(state.params, image[None, ...], mutable=['batch_stats'])[0][0]
+# renormalize to ints in [0, 255]
+reconstruction = (reconstruction - reconstruction.min()) / (reconstruction.max() - reconstruction.min())
+
+plt.subplot(1, 2, 1)
+plt.imshow(image)
+plt.title("Original")
+
+
+plt.subplot(1, 2, 2)
+plt.imshow(reconstruction)
+plt.title("Reconstruction")
+
+# save current figure as image
+plt.savefig("reconstruction.png")
+
+# clear plot
+plt.clf()
+
+# for all 20 blocks, print individual decoding
+z = model.apply(state.params, image[None, ...], mutable=['batch_stats'], method=model.encode)[0]
+decoded_blocks = model.apply(state.params, z, mutable=['batch_stats'], method=model.decode_individual_blocks)[0]
+
+fig = plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
+
+for i in range(num_blocks):
+    ax = fig.add_subplot(2, num_blocks // 2, i+1)
+    # renormalize to [0, 1]
+    decoded_blocks_show = (decoded_blocks[i] - decoded_blocks[i].min()) / (decoded_blocks[i].max() - decoded_blocks[i].min())
+    ax.imshow(decoded_blocks_show[0])
+    ax.set_title(f"Block {i}")
+
+plt.tight_layout()  # Add this line to automatically adjust subplot positions
+
+plt.savefig("blocks.png")
