126ff8125e0104deda6ca3a3e3be94c6628c71aa
michael-psenka <michael.psenka@gmail.com>
2024-03-25 12:09:21 -0400

    added standard transformer encoder/decoder

--- pytorch/slot-attention-pytorch/model.py
+++ pytorch/slot-attention-pytorch/model.py
@@ -8,6 +8,34 @@ from sklearn.cluster import AgglomerativeClustering
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+# standard transformer encoder/decoder using torch.nn
+class TokenEncoder(nn.Module):
+    def __init__(self, emb_size=256, depth=4, heads=8, mlp_dim=512, max_seq_length=512):
+        super().__init__()
+        self.pos_embedding = nn.Parameter(torch.zeros(1, max_seq_length, emb_size))
+        layer = nn.TransformerEncoderLayer(d_model=emb_size, nhead=heads, dim_feedforward=mlp_dim, activation="relu")
+        self.transformer = nn.TransformerEncoder(layer, num_layers=depth)
+
+    def forward(self, x):
+        seq_length = x.size(1)
+        x =  x + self.pos_embedding[:, :seq_length, :]
+        return self.transformer(x)
+
+class TokenDecoder(nn.Module):
+    def __init__(self, emb_size=256, depth=4, heads=8, mlp_dim=512, max_seq_length=512):
+        super().__init__()
+        self.pos_embedding = nn.Parameter(torch.zeros(1, max_seq_length, emb_size))
+        layer = nn.TransformerDecoderLayer(d_model=emb_size, nhead=heads, dim_feedforward=mlp_dim, activation="relu")
+        self.transformer = nn.TransformerDecoder(layer, num_layers=depth)
+
+    def forward(self, z):
+        seq_length = z.size(1)
+        z = z + self.pos_embedding[:, :seq_length, :]
+        # In a typical autoencoder, the memory and target could be the same. However, for simplicity, we're just using the encoded representation directly.
+        # An additional token for the start of sequence might be added, and similarly for the target during training.
+        memory = z.detach().clone()
+        return self.transformer(z, memory)
+
 class SlotAttention(nn.Module):
     def __init__(self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128):
         super().__init__()
